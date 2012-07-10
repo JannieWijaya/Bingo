03905f8b326d9503ef214e3ea98df8bff78488d9
Jannie Wijaya <jannie.wijaya@gmail.com>
2012-07-11 07:05:51 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bingo::Application.config.secret_token = '6bafae79f7db8a1473ee6329ebe4a112194c0c241e9406728015ecb521546b798bba4c495d451b4770b1ac4700ffae49c99bf636877e482cd5e5ba3390b0e27c'
